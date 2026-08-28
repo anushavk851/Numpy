74d429fe58666d02d4330a1b790307bfb60cca78
Anusha VK <anushavk851@gmail.com>
2026-08-29 00:25:26 +0530

    Implement aggregate functions for NumPy array
    Added various aggregate function calculations for a NumPy array.

---
+++ Aggregate Functions/simple_problems.py
@@ -0,0 +1,28 @@
+a = np.array([10, 20, 30, 40, 50])
+
+# 1 Find the sum of all elements.
+print(np.sum(a))
+
+# 2 Find the maximum value.
+print(np.max(a))
+
+# 3 Find the minimum value.
+print(np.min(a))
+
+# 4 Find the average of the array.
+print(np.average(a))
+
+# 5 Find the median.
+print(np.median(a))
+
+# 6 Find the standard deviation.
+print(np.std(a))
+
+# 7 Find the variance.
+print(np.var(a))
+
+# 8 Find the index of the minimum value.
+print(np.argmin(a))
+
+# 9 Find the index of the maximum value
+print(np.argmax(a))
