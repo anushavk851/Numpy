519515097411739e6f2c6dc5b1797925a0718445
Anusha VK <anushavk851@gmail.com>
2026-09-01 23:13:38 +0530

    Update full_function.py

--- special_array_function/full_function.py
+++ special_array_function/full_function.py
@@ -1,3 +1,7 @@
 #to create a constant array
 arr3=np.full((5,6),8,dtype=int) #(5,6)-shape,8-constant value
 print(arr3)
+
+# Create a 3 × 5 array filled with the value 7.
+a=np.full((3,5),7,dtype=int)
+print(a)
