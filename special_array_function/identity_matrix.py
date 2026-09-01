c1650665f66adc79948a0676930e3f842e6c4a4f
Anusha VK <anushavk851@gmail.com>
2026-09-01 23:11:45 +0530

    Add functionality to create and modify identity matrix
    Create a 4x4 identity matrix and set diagonal elements to 5.

--- special_array_function/identity_matrix.py
+++ special_array_function/identity_matrix.py
@@ -4,3 +4,8 @@ arr4=np.eye(3)   #in case of sqaure matrix
 print(arr4)
 arr=np.eye(4,3)
 print(arr)
+
+# 1. Create a 4 × 4 identity matrix and change all diagonal elements to 5.
+arr=np.eye((4),dtype=int)
+b=arr*5
+print(b)
