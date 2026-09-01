12e017e5d64c030a0e4ab65d08218e554fe5af2d
Anusha VK <anushavk851@gmail.com>
2026-09-01 23:28:49 +0530

    Add quantile calculations for a numpy array

---
+++ Control Tendency/quantile.py
@@ -0,0 +1,16 @@
+a=np.array([10,20,30,40,50])
+
+#0th quantile
+print("0th quantile:",np.quantile(a,0))
+
+#25th quantile
+print("0.25 th quantile:",np.quantile(a,0.25))
+
+#50th quantile
+print("0.5 th quantile:",np.quantile(a,0.50))
+
+#75th quantile
+print("0.75th quantile:",np.quantile(a,0.75))
+
+#100th quantile 
+print("1th quantile:",np.quantile(a,1))
