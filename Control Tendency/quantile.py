ad4bfa35c4321194a86c5c28288b4a50e681be44
Anusha VK <anushavk851@gmail.com>
2026-09-02 09:48:29 +0530

    Add quantile calculations for 0.25 and 0.50
    Added calculations for 0.25th and 0.50th quantiles.

--- Control Tendency/quantile.py
+++ Control Tendency/quantile.py
@@ -14,3 +14,9 @@ print("0.75th quantile:",np.quantile(a,0.75))
 
 #100th quantile 
 print("1th quantile:",np.quantile(a,1))
+
+# Find the 0.25 ,0.50 and 0.75 quantile
+a = np.array([5, 10, 15, 20, 25, 30])
+print("0.25th quantile:",np.quantile(a,0.25))
+print("0.50th quantile:",np.quantile(a,0.50))
+print("0.75th quantile:",np.quantile(a,0.75))
