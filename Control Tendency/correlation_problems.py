ab146c8b5935e62e67df1c83beba03af1377c04a
Anusha VK <anushavk851@gmail.com>
2026-09-02 10:02:44 +0530

    Implement correlation coefficient calculations
    Added code to calculate correlation coefficients for two datasets.

---
+++ Control Tendency/correlation_problems.py
@@ -0,0 +1,9 @@
+# Find correlation coefficient
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 4, 6, 8, 10])
+print("correlation",np.corrcoef(x,y))
+
+# Find correlation coefficient
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([10, 8, 6, 4, 2])
+print("correlation",np.corrcoef(x,y))
