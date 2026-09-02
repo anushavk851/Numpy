32f87ec4478c9afe29868d9527087968de97f6ee
Anusha VK <anushavk851@gmail.com>
2026-09-02 09:59:53 +0530

    Add mean calculation to variance and standard deviation script
    Added calculations for mean, variance, and standard deviation for a new dataset.

--- Control Tendency/variance&standard_deviation.py
+++ Control Tendency/variance&standard_deviation.py
@@ -2,3 +2,10 @@
 a = np.array([10, 20, 30, 40, 50])
 print("Varience:",np.var(a))
 print("standard deviation:",np.std(a))
+
+
+# Find Mean, variance and standard deviation
+a = np.array([12, 15, 18, 20, 25, 30])
+print("mean:",np.mean(a))
+print("varience:",np.var(a))
+print("standard deviation:",np.std(a))
